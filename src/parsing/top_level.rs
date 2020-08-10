64f118c4432a0529acff1115bee5db54b1ed4d1e
David L. Inman <davidlevere@gmail.com>
2020-08-09 22:45:59 -0400

    test function definition parser

--- src/parsing/top_level.rs
+++ src/parsing/top_level.rs
@@ -117,5 +117,22 @@ impl<'a> Input<'a> {
 mod test {
     use super::*;
 
+    #[test]
+    fn should_parse_fun_def() -> Result<(), ParseError> {
+        let i = r#"
+fun blah() {
+    return 0;
+}"#.char_indices().collect::<Vec<(usize, char)>>();
+        let mut input = Input::new(&i);
+        let u = input.parse_fun_def()?;
+        
+        assert_eq!( u.name.value, "blah" );
+        assert_eq!( u.type_params.len(), 0 );
+        assert_eq!( u.params.len(), 0 );
+        assert_eq!( u.definition.len(), 1 );
+        assert!( matches!( u.return_type, Type::Unit ) );
+
+        Ok(())
+    }
     
 }
