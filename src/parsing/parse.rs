3b505a748f03ec83a0c1647f5ec3cb27bb6abbd1
David L. Inman <davidlevere@gmail.com>
2020-08-11 23:50:21 -0400

    got rid of todo

--- src/parsing/parse.rs
+++ src/parsing/parse.rs
@@ -7,8 +7,7 @@ use super::input::Input;
 pub fn parse_proc(s : &str) -> Result<Mod, ParseError> {
     let ci = s.char_indices().collect::<Vec<(usize, char)>>();
     let mut input = Input::new(&ci);
-    // TODO if everything fails then the zero or more is going to return no errors, which means we won't be able to show the 
-    // user any sort of errors
+
     let top_level_items = input.zero_or_more(|i| i.parse_top_level() )?; 
     let mut fun_defs = vec![];
     let mut fun_exports = vec![];
