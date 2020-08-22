7bc3a1db34277a43415f051358664c0f484d0929
David L. Inman <davidlevere@gmail.com>
2020-08-22 19:27:18 -0400

    parser test

--- src/parsing/proc/parser.rs
+++ src/parsing/proc/parser.rs
@@ -66,5 +66,41 @@ pub fn parse(s : &str) -> Result<Mod, ParseError> {
 #[cfg(test)]
 mod test {
     use super::*;
+    
+    #[test]
+    fn should_parse_program() -> Result<(), ParseError> {
+        let input = r#"
+use import::{*};
+use import::other::{*};
+use import::other::{*};
+use import::{First, Second, Third};
 
+pub fun fun_name<Type, Type>( first : First, second : Second ) -> path::Type<path::Type> {
+    
+    return t;
+}
+
+pub enum Type { 
+    Case1,
+    Case2
+}
+
+pub struct Name<Type, Type> {
+    first : First<Type>,
+    second : fun(A, B, C) -> Res
+}
+"#;
+
+        let module = parse(input)?;
+
+        assert_eq!( module.uses.len(), 4 );
+        assert_eq!( module.fun_defs.len(), 1 );
+        assert_eq!( module.fun_exports.len(), 1 );
+        assert_eq!( module.enum_defs.len(), 1 );
+        assert_eq!( module.enum_exports.len(), 1 );
+        assert_eq!( module.struct_defs.len(), 1 );
+        assert_eq!( module.struct_exports.len(), 1 );
+
+        Ok(())
+    }
 }
